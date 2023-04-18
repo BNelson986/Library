990b6f4ecc8216e796bc8abb2209de731a310774
Bryan <bryan.nelson20@yahoo.com>
2023-04-18 14:02:34 -0700

    Redesigned script to exclude books with no ISBN

--- reduceFileSize.py
+++ reduceFileSize.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 
@@ -14,20 +15,27 @@ fileOut = open(outputFile, 'a')
 with open(inputFile, mode='r') as fileIn:
     #   Process 44131073 lines one by one
     for line in fileIn:
-        lines.append(line)
-        i += 1
-        #   Once certain number of lines appended, shuffle section
-        if i > 11032768:
-            random.shuffle(lines)
-            #   Number of lines to add from each randomized group
-            num_lines = 125000
-            fileOut.writelines(lines[:num_lines])
-            #   Clear lines list to free memory, reset counter variable
-            items += num_lines
-            lines.clear()
-            i = 0
-            print(f"Roughly {items} Copied")
+       
+        line = line[line.find('{'):]
+        
+        #   Verify if no isbn is present, skip entry
+        if 'isbn_10' not in line and 'isbn_13' not in line:
             continue
+        else:
+            lines.append(line)
+            i += 1
+            #   Once certain number of lines appended, shuffle section
+            if i > 11032768:
+                random.shuffle(lines)
+                #   Number of lines to add from each randomized group
+                num_lines = 125000
+                fileOut.writelines(lines[:num_lines])
+                #   Clear lines list to free memory, reset counter variable
+                items += num_lines
+                lines.clear()
+                i = 0
+                print(f"Roughly {items} Copied")
+                continue
 
     #   Final group of the file
     random.shuffle(lines)
